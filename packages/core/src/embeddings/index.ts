738375dc4892a5236eaa2fc97f564ec9e9794929
dingyi <dingyi222666@foxmail.com>
2025-02-19 01:38:34 +0800

    feat(core): implement batch processing for embedding model
    - Add support for batch processing in embedding model
    - Introduce new types and functions for handling batch embedding calls
    - Implement chunking logic to process large arrays in batches
    - Update embedding usage calculation to handle multiple batches

--- packages/core/src/embeddings/index.ts
+++ packages/core/src/embeddings/index.ts
@@ -5,18 +5,43 @@ export type EmbeddingModel<T = string> = {
 
     readonly batchSize: number | undefined
 
-    doEmbed<R extends T | T[] = T>(options: {
-        values: R
-        modelId?: string
-        signal?: AbortSignal
-        headers?: Record<string, string>
-    }): PromiseLike<{
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        embeddings: R extends (infer U)[] ? number[][] : number[]
-        usage?: EmbeddingModelUsage
-    }>
+    doEmbed<R extends T | T[] = T>(
+        options: EmbeddingModelCallOptions<R>
+    ): PromiseLike<EmbeddingModelResult<R>>
 }
 
 export type EmbeddingModelUsage = {
     tokens: number
+}
+
+export type EmbeddingModelCallSettings = {
+    batchSize: number
+}
+
+export type EmbeddingModelResult<R> = {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    embeddings: R extends (infer U)[] ? number[][] : number[]
+    usage?: EmbeddingModelUsage
+}
+
+export type EmbeddingModelCallOptions<T> = {
+    values: T
+    modelId?: string
+    signal?: AbortSignal
+    headers?: Record<string, string>
+} & Partial<EmbeddingModelCallSettings>
+
+export function addEmbeddingModelUsage(
+    left?: EmbeddingModelUsage,
+    right?: EmbeddingModelUsage
+) {
+    if (left == null) {
+        return right
+    }
+    if (right == null) {
+        return left
+    }
+    return {
+        tokens: left.tokens + right.tokens
+    }
 }
--- packages/core/src/generate/embed.ts
+++ packages/core/src/generate/embed.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { EmbeddingModel, EmbeddingModelUsage } from '../embeddings/index.ts'
+import {
+    addEmbeddingModelUsage,
+    EmbeddingModel,
+    EmbeddingModelUsage
+} from '../embeddings/index.ts'
+import { chunkArray } from '../utils/chunk-array.ts'
 
-export async function doEmbed<T = string>({
+export async function embed<T = string>({
     model,
     value,
     signal,
@@ -12,21 +17,52 @@ export async function doEmbed<T = string>({
     signal?: AbortSignal
     headers?: Record<string, string>
 }): Promise<EmbedResult<T>> {
-    const response = await model.doEmbed({
-        values: Array.isArray(value) ? value : [value],
-        signal,
-        headers
-    })
+    if (
+        (model.batchSize != null &&
+            Array.isArray(value) &&
+            value.length < model.batchSize) ||
+        (Array.isArray(value) && value.length < 20) ||
+        !Array.isArray(value)
+    ) {
+        const response = await model.doEmbed({
+            values: Array.isArray(value) ? value : [value],
+            signal,
+            headers
+        })
 
-    return {
-        embedding: Array.isArray(value)
-            ? (response.embeddings as number[][] as any)
-            : (response.embeddings[0] as number[] as any),
-        value,
-        usage: response.usage ?? {
-            tokens: NaN
+        return {
+            embedding: Array.isArray(value)
+                ? (response.embeddings as number[][] as any)
+                : (response.embeddings[0] as number[] as any),
+            value,
+            usage: response.usage ?? {
+                tokens: NaN
+            }
         }
     }
+
+    return await chunkArray(value, (model.batchSize ?? 21) - 1).reduce(
+        async (acc, chunk) => {
+            const result = await acc
+            const response = await model.doEmbed({
+                values: chunk,
+                signal,
+                headers
+            })
+            return {
+                embedding: result.embedding.concat(response.embeddings),
+                value,
+                usage: addEmbeddingModelUsage(result.usage, response.usage)
+            } as EmbedResult<T>
+        },
+        Promise.resolve({
+            embedding: [] as number[][],
+            value,
+            usage: {
+                tokens: NaN
+            }
+        } as EmbedResult<T>)
+    )
 }
 
 export interface EmbedResult<T> {
